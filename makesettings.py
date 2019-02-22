a9741bbc642ac47e1219992bc3749efa794680f6
Brian Ballsun-Stanton <brian@fedarch.org>
2019-02-22 18:52:40 +1100

    JTI detection

--- makesettings.py
+++ makesettings.py
@@ -7,6 +7,7 @@ with open("secrets.json", "w") as sec:
 	secret = {'default_institution':"",
 			  'base_url': "https://localhost:8443/",
 			  'aaf_url': "",
+			  'issuer': "https://rapid.test.aaf.edu.au",			  
 			  'jwt_endpoint_path': '/auth/jwt'
 			  }
 	secret['token'] = secrets.token_urlsafe(64)
